05b31a65b11862e6210ed9c673f49919483c6bc4
Subhajit Sahu <wolfram77@gmail.com>
2021-05-19 15:07:15 +0530

    :skunk: w/ _cuda

---
+++ src/_cuda.hxx
@@ -0,0 +1,105 @@
+#pragma once
+#include <cstdio>
+#include <cstdlib>
+#include <cuda_runtime.h>
+#include <cuda_profiler_api.h>
+
+using std::fprintf;
+using std::exit;
+
+
+
+
+// TRY
+// ---
+// Log error if CUDA function call fails.
+
+#ifndef TRY_CUDA
+void tryCuda(cudaError err, const char* exp, const char* func, int line, const char* file) {
+  if (err == cudaSuccess) return;
+  fprintf(stderr,
+    "%s: %s\n"
+    "  in expression %s\n"
+    "  at %s:%d in %s\n",
+    cudaGetErrorName(err), cudaGetErrorString(err), exp, func, line, file);
+  exit(err);
+}
+
+#define TRY_CUDA(exp) tryCuda(exp, #exp, __func__, __LINE__, __FILE__)
+#endif
+
+#ifndef TRY
+#define TRY(exp) TRY_CUDA(exp)
+#endif
+
+
+
+
+// DEFINE
+// ------
+// Define thread, block variables.
+
+#ifndef DEFINE_CUDA
+#define DEFINE_CUDA(t, b, B, G) \
+  int t = threadIdx.x; \
+  int b = blockIdx.x; \
+  int B = blockDim.x; \
+  int G = gridDim.x;
+#define DEFINE_CUDA2D(tx, ty, bx, by, BX, BY, GX, GY) \
+  int tx = threadIdx.x; \
+  int ty = threadIdx.y; \
+  int bx = blockIdx.x; \
+  int by = blockIdx.y; \
+  int BX = blockDim.x; \
+  int BY = blockDim.y; \
+  int GX = gridDim.x;  \
+  int GY = gridDim.y;
+#endif
+
+#ifndef DEFINE
+#define DEFINE(t, b, B, G) \
+  DEFINE_CUDA(t, b, B, G)
+#define DEFINE2D(tx, ty, bx, by, BX, BY, GX, GY) \
+  DEFINE_CUDA2D(tx, ty, bx, by, BX, BY, GX, GY)
+#endif
+
+
+
+
+// UNUSED
+// ------
+// Mark CUDA kernel variables as unused.
+
+template <class T>
+__device__ void unusedCuda(T&&) {}
+
+#ifndef UNUSED_CUDA
+#define UNUSED_CUDA(...) ARG_CALL(unusedCuda, ##__VA_ARGS__)
+#endif
+
+#ifndef UNUSED
+#define UNUSED UNUSED_CUDA
+#endif
+
+
+
+
+// REMOVE IDE SQUIGGLES
+// --------------------
+
+#ifndef __SYNCTHREADS
+void __syncthreads();
+#define __SYNCTHREADS() __syncthreads()
+#endif
+
+#ifndef __global__
+#define __global__
+#endif
+
+#ifndef __device__
+#define __device__
+#endif
+
+#ifndef __shared__
+#define __shared__
+#endif
--- src/_main.hxx
+++ src/_main.hxx
@@ -2,6 +2,7 @@
 #include "_algorithm.hxx"
 #include "_cmath.h"
 #include "_ctypes.hxx"
+#include "_cuda.hxx"
 #include "_iostream.hxx"
 #include "_iterator.hxx"
 #include "_utility.hxx"
