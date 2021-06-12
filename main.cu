3b1213aa78329834ade091c2427706f97c7f6682
Subhajit Sahu <wolfram77@gmail.com>
2021-06-12 17:34:58 +0530

    :skunk: w/ expt

--- main.cu
+++ main.cu
@@ -9,10 +9,10 @@ using namespace std;
 
 
 #define REPEAT 5
-#define SORT_VERTICES true
 
 template <class G, class H>
 void runPagerank(const G& x, const H& xt, bool show) {
+  typedef PagerankSort Sort;
   vector<float> *init = nullptr;
 
   // Find pagerank using nvGraph.
@@ -20,15 +20,16 @@ void runPagerank(const G& x, const H& xt, bool show) {
   auto e1 = l1Norm(a1.ranks, a1.ranks);
   printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankNvgraph\n", a1.time, a1.iterations, e1);
 
-  // Find pagerank using CUDA thread-per-vertex.
-  auto a2 = pagerankCuda(xt, init, {REPEAT});
-  auto e2 = l1Norm(a2.ranks, a1.ranks);
-  printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankCuda\n", a2.time, a2.iterations, e2);
-
-  // Find pagerank using CUDA thread-per-vertex, with vertices sorted by in-degree.
-  auto a3 = pagerankCuda(xt, init, {REPEAT, SORT_VERTICES});
-  auto e3 = l1Norm(a3.ranks, a1.ranks);
-  printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankCuda [vert-indeg]\n", a3.time, a3.iterations, e3);
+  for (int vertex=0; vertex<3; vertex++) {
+    for (int edge=0; edge<3; edge++) {
+      Sort sortVertex = static_cast<Sort>(vertex);
+      Sort sortEdge   = static_cast<Sort>(edge);
+      // Find pagerank using CUDA block-per-vertex.
+      auto a2 = pagerankCuda(xt, init, {REPEAT, sortVertex, sortEdge});
+      auto e2 = l1Norm(a2.ranks, a1.ranks);
+      printf("[%09.3f ms; %03d iters.] [%.4e err.] pagerankCuda [sortv=%s; sorte=%s]\n", a2.time, a2.iterations, e2, stringify(sortVertex).c_str(), stringify(sortEdge).c_str());
+    }
+  }
 }
 
 
