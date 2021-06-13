abf33fe930d2253e5c130f9ead621f3d58fb929d
Subhajit Sahu <wolfram77@gmail.com>
2021-06-13 16:42:11 +0530

    :skunk: w/ expt

--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-src="pagerank-cuda-switched-adjust-block-launch"
+src="pagerank-cuda-switched-adjust-thread-launch"
 out="/home/resources/Documents/subhajit/$src.log"
 ulimit -s unlimited
 printf "" > "$out"
